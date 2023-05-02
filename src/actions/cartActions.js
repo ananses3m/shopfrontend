e09ffa0a04a29a36a5574b7e42afd2a1c57bc531
ananses3m <123345707+ananses3m@users.noreply.github.com>
2023-05-01 22:32:17 -0600

    Update cartActions.js

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
 
-    const { data } = await axios.get(`/api/products/${id}`)
+    const { data } = await axios.get(`https://anansesem-shop-backend.onrender.com/api/products/${id}`)
 
     dispatch({
         type: CART_ADD_ITEM,
